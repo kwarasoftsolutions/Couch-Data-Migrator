96ee66902832647110c987b71578f1ae396c1b35
Adebiyi <E1711900@mh.org>
2016-10-26 12:54:42 -0500

    added repository and service

---
+++ repository/couch_repository.js
@@ -0,0 +1,95 @@
+﻿'use strict';
+var couch = require('../scripts/db.js');
+
+var CouchRepository = function () {
+
+    this.getDocumentById = function (documentId, cb) {
+
+        couch.get(documentId, function (err, doc) {
+            if (err) {
+                cb(err, null);
+            } else {
+                cb(null, doc);
+            }
+        });
+
+    },
+    this.getDocumentView = function (view,cb) {
+        couch.view(view, function (err, res) {
+            if (err) {
+                cb(err, null);
+            } else {
+                cb(null, res);
+            }
+        });
+    },
+     this.searchDocumentView = function (view,searchParams, cb) {
+         couch.view(view,searchParams, function (err, res) {
+             if (err) {
+                 cb(err, null);
+             } else {
+                 cb(null, res);
+             }
+         });
+     },
+    this.addDocument = function(data,cb)
+    {
+        if(data._id)
+        {
+            couch.save(data._id, data, function (err, doc) {
+                if(err){
+                    cb(err, null);
+                } else {
+                    cb(null, doc);
+                }
+            });
+        } else {
+            couch.save(data, function (err, res) {
+                if (err) {
+                    cb(err, null);
+                } else {
+                    cb(null, res);
+                }
+            });
+        }
+    },
+    this.updateDocument = function(documentId,data,cb)
+    {
+        couch.get(documentId, function (err, doc) {
+            if (err) {
+                cb(err, null);
+            } else {
+                
+                var revId = doc.rev;
+                couch.save(documentId, revId, data, function (err, res) {
+                    if (err) {
+                        cb(err, null);
+                    } else {
+                        cb(null, res);
+                    }
+                })
+            }
+        });
+    },
+    this.deleteDocument = function(documentId,cb)
+    {
+        couch.get(documentId, function (err, doc) {
+            if (err) {
+                cb(err, null);
+            } else {
+
+                var revId = doc.rev;
+                couch.remove(documentId, revId,function (err, res) {
+                    if (err) {
+                        cb(err, null);
+                    } else {
+                        cb(null, res);
+                    }
+                })
+            }
+        });
+    }
+};
+
+
+module.exports = CouchRepository;
---
+++ services/data_service.js
@@ -0,0 +1,73 @@
+﻿'use strict';
+
+var CouchRepository = require('../repository/couch_repository'),
+    repository = new CouchRepository();
+
+var DataService = function () {
+    this.getById = function (documentId, cb) {
+       return repository.getDocumentById(documentId, function (err, response) {
+            if (err) {
+                cb(err, null);
+            }
+            else {
+                cb(null, response);
+            }
+        })
+    },
+    this.getAll = function (viewName, cb) {
+        return repository.getDocumentView(viewName, function (err, response) {
+            if (err) {
+                cb(err, null);
+            }
+            else {
+                cb(null, response);
+            }
+        })
+    }
+    ,
+    this.create = function (data, cb) {
+
+        return repository.addDocument(data, function (err, response) {
+            if (err) {
+                cb(err, null);
+            } else {
+                cb(null, response);
+            }
+        });
+    },
+     this.update = function (documentId, data, cb) {
+         return repository.updateDocument(documentId, data, function (err, response) {
+             if (err) {
+                 cb(err, null);
+             } else {
+                 cb(null, response);
+             }
+         });
+     },
+     this.delete = function (documentId, cb) {
+         return repository.deleteDocument(documentId, function (err, response) {
+             if (err) {
+                 cb(err, null);
+             } else {
+                 cb(null, response);
+             }
+         });
+     }
+};
+
+module.exports = DataService;
+
+//var ds = new DataService();
+
+//var newDoc = {album:'Key Change', artist:'Mocky Jazzist', releasedate: '12/22/2017' }
+
+//ds.delete('3a7a9506c4b33a66c3adb0aa050029b5', function (err, resp) {
+
+//    if (err) {
+//        console.log(err);
+//    } else {
+//       // var doc = { id: resp._id, name: resp.name, email: resp.email };
+//        // console.log(doc);
+//        console.log(resp);
+//    }
+//});
